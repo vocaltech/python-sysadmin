52b5ad5f38b74b118efbc31d1dd55854e2e776a1
vocaltech <it@vocaltech.fr>
2024-02-27 23:55:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## Useful sysadmin tools
---
+++ src/ufw_update_rules.py
@@ -0,0 +1,62 @@
+import os
+import re
+
+####################################################
+#
+# Update some rules with a given remoteIp
+#
+####################################################
+
+#
+# Functions
+#
+def getSSHRemoteIp() -> str:
+  stream = os.popen('echo $SSH_CLIENT')
+  return stream.read().split()[0]
+
+def getRuleId(currentRule: str) -> str:
+  result = re.search(r"(\d+)", currentRule)
+  return result.group(1)
+
+def getUfwRules() -> list:
+  stream = os.popen('ufw status numbered')
+  return stream.readlines()
+
+def deleteRule(port: str):
+  ufwRules = getUfwRules()
+
+  for i in range(len(ufwRules)):
+    if i >= 4:
+      currentRule = ufwRules[i]
+
+      if port in currentRule:
+        ruleId = getRuleId(currentRule)
+        txt = "found " + port + " - ruleId to delete: {}"
+        print(txt.format(ruleId))
+        deleteRule = "echo 'y' | ufw delete " + ruleId
+        os.system(deleteRule)
+
+def addRule(fromIp: str, port: str, protocol: str):
+  addRule = "ufw allow from " + fromIp + " to any port " + port  + " proto " + protocol
+  os.system(addRule)
+
+#
+# main
+#
+
+# Get ssh remote ip address
+txt = "Remote ip addr: {}"
+print(txt.format(getSSHRemoteIp()))
+
+# Update rule with port 5432
+deleteRule("5432")
+addRule(getSSHRemoteIp(), "5432", "tcp")
+
+# Update rule with port 27017
+deleteRule("27017")
+addRule(getSSHRemoteIp(), "27017", "tcp")
+
+# Update rule with port 3306
+deleteRule("3306")
+addRule(getSSHRemoteIp(), "3306", "tcp")
+
